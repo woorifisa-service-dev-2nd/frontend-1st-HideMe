f71e9682f73c0c76a6e4c6b785b86d7754f3181a
GyeongEun-Kim <kekim20@naver.com>
2023-11-25 16:26:01 +0900

    [ADD] : 모자이크 구현(중복)

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const image = document.getElementById('image');
 const canvas = document.getElementById('canvas'); //캔버스 생성
 const ctx = canvas.getContext('2d');
+const imageContext = image.getContext('2d');
 
-const park = new Image();
-park.src = 'images/박진영.jpeg';
+const SQURE_SIZE = 50;
+
+const fubao = new Image();
+fubao.src = "images/푸바오.png";
+console.log(fubao);
+
+
+
+fubao.onload = () => {
+    imageContext.drawImage(fubao, 0, 0, 500, 500);
+}
+
 
 
 //이미지 위치 구하기
@@ -11,14 +22,13 @@ const imageTop = image.getBoundingClientRect().top;
 const imageBottom = image.getBoundingClientRect().bottom;
 const imageLeft = image.getBoundingClientRect().left;
 const imageRight = image.getBoundingClientRect().right;
-//console.log(`${imageTop}  ${imageBottom} ${imageLeft} ${imageRight}`);
 
 
-const drawImage = image.onload = (xPos, yPos) => {
+const drawImage = (xPos, yPos) => {
     // 이미지 그리기
 
-    let dx = 50 * xPos;
-    let dy = 50 * yPos;
+    let dx = SQURE_SIZE * xPos;
+    let dy = SQURE_SIZE * yPos;
 
     const imageWidth = imageRight - imageLeft;
     const imageHeight = imageBottom - imageTop;
@@ -26,24 +36,47 @@ const drawImage = image.onload = (xPos, yPos) => {
 
     console.log(`dx:${dx} dy:${dy}`);
 
-    ctx.drawImage(park, dx, dy, 50, 50, dx, dy, 50, 50);
+    //ctx.drawImage(park, dx, dy, 50, 50, dx, dy, 50, 50);
     //모자이크 하기
+    const randomPixel = getRandomPixel(dx, dy);
+    console.log(randomPixel);
+    ctx.fillStyle = `rgb(${randomPixel.red},${randomPixel.green},${randomPixel.blue})`;
+    ctx.fillRect(dx, dy, SQURE_SIZE, SQURE_SIZE);
 
-    document.body.appendChild(ctx.canvas);
-    //drawImage(image, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight)
+    //document.body.appendChild(ctx.canvas);
 }
+
 
 
 canvas.addEventListener('mousemove', (event) => {
     const x = event.clientX;
     const y = event.clientY;
-    console.log(`x:${x} y: ${y}`);
+    //console.log(`x:${x} y: ${y}`);
 
-    const xPos = parseInt((x - imageLeft) / 50); //왼쪽에서 몇번째 격자인지
-    const yPos = parseInt((y - imageTop) / 50); //위에서 몇번째 격자인지
-    //console.log(`${xPos} ${yPos}`);
+    const xPos = parseInt((x - imageLeft) / SQURE_SIZE); //왼쪽에서 몇번째 격자인지
+    const yPos = parseInt((y - imageTop) / SQURE_SIZE); //위에서 몇번째 격자인지
+
     drawImage(xPos, yPos);
 });
+
+const getRandomPixel = (x, y) => {
+    const randomX = x + parseInt(Math.random() * SQURE_SIZE);
+    const randomY = y + parseInt(Math.random() * SQURE_SIZE);
+    console.log(`randomX : ${randomX} randomY : ${randomY}`);
+    console.log(`number?? ${typeof randomX}`);
+    const imageData = imageContext.getImageData(randomX, randomY, 1, 1).data;
+    console.log(imageData);
+    return {
+        red: imageData[0],
+        green: imageData[1],
+        blue: imageData[2]
+    }
+
+
+}
+
+
+
 
 
 
